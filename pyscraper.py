b23928a9d69d9371adb521f4b32adbc599cd1d54
Indrajeet Gour <igour@slb.com>
2020-05-25 16:13:41 +0530

    Formatted code

--- pyscraper.py
+++ pyscraper.py
@@ -1,19 +1,37 @@
+"""
+Crawler the sample web site data for this example
+"""
+
 import requests
 import bs4
 
-res = requests.get('https://www.developercodehack.com/')
+site_url = 'https://www.developercodehack.com/'
 
-# print(type(res))
 
-# print(res.text)
+def get_site_response():
+    res = requests.get(site_url)
+    return res
 
-soup = bs4.BeautifulSoup(res.text, 'lxml')
-# print(soup.prettify())
-# print(type(soup))
-for article in soup.find_all('div', class_='blog-posts hfeed container index-post-wrap'):
-    # print(article.article.prettify())
-    for oneArticle in article.article.find_all('div', class_='entry-header'):
-        print(oneArticle.h2.a.text)
+
+def extract_site_response(res):
+    # start parsing the site response
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    # print(soup.prettify())
+    for article in soup.find_all('div', class_='blog-posts hfeed container index-post-wrap'):
+        # print(article.article.prettify())
+        for oneArticle in article.article.find_all('div', class_='entry-header'):
+            print(oneArticle.h2.a.text)
+
+
+if __name__ == '__main__':
+    response = get_site_response()
+
+    extract_site_response(response)
+
+
+
+
+
 
 """
 print(soup.select('.widget-content'))
